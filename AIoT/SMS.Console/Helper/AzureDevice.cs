93880f06fa1382090856db54510afbb7415d6255
muhammad ibnu fadhil <mifmasterz@gmail.com>
2023-01-11 05:06:13 +0700

    init:

---
+++ AIoT/SMS.Console/Helper/AzureDevice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices;
+
+namespace SMS.Console.Helper
+{
+	public class AzureDevice
+	{
+        ServiceClient serviceClient;
+        string connectionString { set; get; }
+        string targetDevice { set; get; }
+		public bool IsReady { get; internal set; } = false;
+        public AzureDevice(string ConnectionString, string TargetDevice)
+		{
+			this.connectionString = ConnectionString;
+			this.targetDevice = TargetDevice;
+			Setup();
+		}
+
+		public async Task<bool> SendMessageToDevice(string Message)
+		{
+			if (!IsReady) return false;
+			if (string.IsNullOrEmpty(Message))
+			{
+				System.Console.WriteLine("Please fill message");
+				return false;
+			}
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(Message));
+            await serviceClient.SendAsync(targetDevice, commandMessage);
+			return true;
+        }
+
+		void Setup()
+		{
+			try
+			{
+                serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+				IsReady = true;
+            }
+            catch (Exception ex)
+			{
+				System.Console.WriteLine(ex);
+				IsReady = false;
+			}
+        }
+	}
+}
+
---
+++ AIoT/SMS.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SMS.Console.Helper;
+using Spectre.Console;
+
+AzureDevice device=null;
+AnsiConsole.MarkupLine("[green]{0}[/]", Markup.Escape("Selamat datang di aplikasi [SMS] IoT"));
+// Synchronous
+AnsiConsole.Status()
+    .Start("Mulai proses...", ctx =>
+    {
+        // Simulate some work
+        AnsiConsole.MarkupLine("Mulai koneksi ke Azure IoT...");
+        Thread.Sleep(100);
+        device = new AzureDevice("HostName=BmcIoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1hyvQvLk7mWsQmBJ/MuTxWqiwr1nqtNnBs6gNqspta4=", "myFirstDevice");
+        // Update the status and spinner
+        ctx.Status("Konesi berhasil");
+        ctx.Spinner(Spinner.Known.Star);
+        ctx.SpinnerStyle(Style.Parse("green"));
+        Thread.Sleep(2000);
+       
+    });
+
+while (true && device!=null)
+{
+    var message = AnsiConsole.Ask<string>("Ketik [green]pesan[/] yang mau dikirim! [yellow]('q' untuk keluar)[/]");
+    if (message == "q") break;
+    var hasil = await device.SendMessageToDevice(message);
+    if (hasil)
+    {
+        AnsiConsole.MarkupLine($"pesan [green]berhasil[/] terkirim {Emoji.Known.ThumbsUp}");
+    }
+    else
+    {
+        AnsiConsole.MarkupLine($"pesan [red]gagal[/] terkirim {Emoji.Known.Alien}");
+    }
+    var rule = new Rule("[red]PESAN BARU[/]");
+    AnsiConsole.Write(rule);
+}
+
+AnsiConsole.MarkupLine("Terima kasih, sampai jumpa. :person_running:");
+System.Console.ReadLine();
