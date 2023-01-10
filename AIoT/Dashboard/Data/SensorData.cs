0928a48dea806f0730f9c93b9095988808ac03fd
muhammad ibnu fadhil <mifmasterz@gmail.com>
2023-01-11 06:07:54 +0700

    dashboard

---
+++ AIoT/Dashboard/Data/SensorData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Dashboard.Data
+{
+    public class SensorData
+    {
+        public double Temp { get; set; }
+        public double Light { get; set; }
+        public double Pressure { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
+
+
